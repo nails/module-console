e218a186515f7131bf160d63b8c44c395f09e31d
Pablo de la Peña <p@shedcollective.org>
2017-03-06 11:34:00 +0000

    Simplified services
    Encourage the sue of loading the database class using `Database` rather than `ConsoleDatabase`

--- services/services.php
+++ services/services.php
@@ -2,9 +2,21 @@
 
 return array(
     'services' => array(
+        'Database' => function () {
+            //  @todo - Use the common Database driver once it is not dependant on CI
+            if (class_exists('\App\Console\Database')) {
+                return new \App\Console\Database();
+            } else {
+                return new \Nails\Console\Database();
+            }
+        },
         'ConsoleDatabase' => function () {
-            //  @todo: Use the common Database driver once it is not dependant on CI
-            return new \Nails\Console\Database();
+            //  Alias of above
+            if (class_exists('\App\Console\Database')) {
+                return new \App\Console\Database();
+            } else {
+                return new \Nails\Console\Database();
+            }
         }
     )
 );
