4c84de06e565ea66a0beb62ac9e021ca80898b8a
Pablo de la Peña <p@shedcollective.org>
2018-11-19 14:14:16 +0000

    Moving more of the condole logic into the App class

--- console.php
+++ console.php
@@ -21,32 +21,10 @@ use Nails\Console\App;
 $sEntryPoint = realpath(__DIR__ . '/../../../') . '/index.php';
 require_once dirname($sEntryPoint) . '/vendor/autoload.php';
 
-
-/*
- *---------------------------------------------------------------
- * App Bootstrapper: preSystem
- *---------------------------------------------------------------
- *
- * Allows the app to execute code very early on in the console tool lifecycle
- *
- */
-if (class_exists('App\Console\Bootstrap') && is_callable('\App\Console\Bootstrap::preSystem')) {
-    \App\Console\Bootstrap::preSystem();
-}
-
-
-/*
- *---------------------------------------------------------------
- * Nails Bootstrapper
- *---------------------------------------------------------------
- */
-\Nails\Bootstrap::run($sEntryPoint);
-
-
 /*
  *---------------------------------------------------------------
  * Run App
  *---------------------------------------------------------------
  */
 $oApp = new App();
-$oApp->go();
+$oApp->go($sEntryPoint);
--- src/App.php
+++ src/App.php
@@ -6,11 +6,46 @@ use Nails\Common\Service\ErrorHandler;
 use Nails\Console\Utf8;
 use Nails\Factory;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 final class App
 {
-    public function go()
-    {
+    /**
+     * Execues the console app
+     *
+     * @param string               $sEntryPoint      The path of the route index.php file
+     * @param InputInterface|null  $oInputInterface  The input interface to use
+     * @param OutputInterface|null $oOutputInterface The output interface to use
+     * @param bool                 $bAutoExit        Whether to autoexit from the application or not
+     *
+     * @throws \Nails\Common\Exception\FactoryException
+     */
+    public function go(
+        $sEntryPoint,
+        InputInterface $oInputInterface = null,
+        OutputInterface $oOutputInterface = null,
+        $bAutoExit = true
+    ) {
+        /*
+         *---------------------------------------------------------------
+         * App Bootstrapper: preSystem
+         *---------------------------------------------------------------
+         * Allows the app to execute code very early on in the console tool lifecycle
+         */
+        if (class_exists('App\Console\Bootstrap') && is_callable('\App\Console\Bootstrap::preSystem')) {
+            \App\Console\Bootstrap::preSystem($this);
+        }
+
+
+        /*
+         *---------------------------------------------------------------
+         * Nails Bootstrapper
+         *---------------------------------------------------------------
+         */
+        \Nails\Bootstrap::run($sEntryPoint);
+
+
         /*
          *---------------------------------------------------------------
          * Command line can run forever
@@ -27,6 +62,7 @@ final class App
          */
         $oUtf8 = new Utf8();
 
+
         /*
          *---------------------------------------------------------------
          * CLI only
@@ -40,11 +76,25 @@ final class App
 
         /*
          *---------------------------------------------------------------
+         * Instanciate the application
+         *---------------------------------------------------------------
+         */
+        $oApp = new Application();
+
+
+        /*
+         *---------------------------------------------------------------
+         * Set auto-exit behaviour
+         *---------------------------------------------------------------
+         */
+        $oApp->setAutoExit($bAutoExit);
+
+
+        /*
+         *---------------------------------------------------------------
          * Command Locations
          *---------------------------------------------------------------
-         *
          * Define which directories to look in for Console Commands
-         *
          */
 
         $aCommandLocations = [
@@ -60,13 +110,12 @@ final class App
             ];
         }
 
+
         /*
          *---------------------------------------------------------------
          * Load Commands
          *---------------------------------------------------------------
-         *
          * Recursively look for commands
-         *
          */
 
         Factory::helper('directory');
@@ -93,53 +142,50 @@ final class App
             findCommands($aCommands, $sPath, $sNamespace);
         }
 
-        /*
-         *---------------------------------------------------------------
-         * Instanciate the application
-         *---------------------------------------------------------------
-         *
-         * Instanciate the application and add commands
-         *
-         */
-        $oApp = new Application();
         foreach ($aCommands as $sCommandClass) {
             $oApp->add(new $sCommandClass());
         }
+
 
         /*
          *---------------------------------------------------------------
          * App Bootstrapper: preCommand
          *---------------------------------------------------------------
-         *
          * Allows the app to execute code just before the command is called
-         *
          */
         if (class_exists('App\Console\Bootstrap') && is_callable('\App\Console\Bootstrap::preCommand')) {
-            \App\Console\Bootstrap::preCommand();
+            \App\Console\Bootstrap::preCommand($this);
         }
+
 
         /*
          *---------------------------------------------------------------
          * Run the application
          *---------------------------------------------------------------
-         *
          * Go, go, go!
-         *
          */
+        $oApp->run($oInputInterface, $oOutputInterface);
 
-        $oApp->run();
 
         /*
          *---------------------------------------------------------------
          * App Bootstrapper: postCommand
          *---------------------------------------------------------------
-         *
          * Allows the app to execute code just after the command is called
-         *
          */
         if (class_exists('App\Console\Bootstrap') && is_callable('\App\Console\Bootstrap::postCommand')) {
-            \App\Console\Bootstrap::postCommand();
+            \App\Console\Bootstrap::postCommand($this);
         }
 
+
+        /*
+         *---------------------------------------------------------------
+         * App Bootstrapper: postSystem
+         *---------------------------------------------------------------
+         * Allows the app to execute code at the very end of the console tool lifecycle
+         */
+        if (class_exists('App\Console\Bootstrap') && is_callable('\App\Console\Bootstrap::postSystem')) {
+            \App\Console\Bootstrap::postSystem($this);
+        }
     }
 }
