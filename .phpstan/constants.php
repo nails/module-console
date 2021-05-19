f98a58dfde0cc65a12de33609c7784bd907cb3b3
Pablo de la Peña <p@shedcollective.org>
2021-05-19 14:37:10 +0100

    Use realpath in PHPStan

--- .phpstan/constants.php
+++ .phpstan/constants.php
@@ -1,4 +1,4 @@
 <?php
-define('NAILS_CI_SYSTEM_PATH', __DIR__ . '/../vendor/codeigniter/framework/system/');
+define('NAILS_CI_SYSTEM_PATH', realpath(__DIR__ . '/../vendor/codeigniter/framework/system') . '/');
 define('BASEPATH', '');
 define('NAILS_APP_PATH', '');
