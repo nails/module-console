0f9a082dd2a4d23cb1c09c15f745d2bdfcb64418
Pablo de la Peña <p@shedcollective.org>
2025-04-17 16:21:19 +0100

    chore: Update paths

--- .phpstan/constants.php
+++ .phpstan/constants.php
@@ -1,4 +1,4 @@
 <?php
-define('NAILS_CI_SYSTEM_PATH', realpath(__DIR__ . '/../vendor/codeigniter/framework/system') . '/');
+define('NAILS_CI_SYSTEM_PATH', realpath(__DIR__ . '/../vendor/pocketarc/codeigniter/system') . '/');
 define('BASEPATH', '');
 define('NAILS_APP_PATH', '');
